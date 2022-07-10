74e52b58ac7da11b2d268c6885c166978e7002fe
Szedof <48103543+Szedof@users.noreply.github.com>
2022-07-10 22:32:34 +0200

    Add files via upload

---
+++ extension/background.js
@@ -0,0 +1,53 @@
+console.log("From background");
+let tabID = 0
+let ref = null;
+let options = {}
+
+chrome.runtime.onMessage.addListener(request => {
+    tabID = request.tabIDMessage;
+    options = request.options;
+    console.log(tabID)
+    if(request.message == "start"){
+        refresh(tabID, options.webRefreshTime)
+        clickBtn(tabID, options.btnPath, options.btnClickTime)
+    } else {
+        stopRef()
+    }
+})
+
+chrome.webNavigation.onCommitted.addListener((info) => {
+    if(info.transitionType == "reload" && options.btnPath != "") {
+        clickBtn(tabID, options.btnPath, options.btnClickTime);
+    }
+});
+
+const refresh = (id) => {
+    ref = setInterval(() => {
+        clickBtn(tabID, options.btnPath, options.btnClickTime)
+        chrome.tabs.reload(id)
+    }, options.webRefreshTime);
+}
+
+const stopRef = () => {
+    clearInterval(ref)
+}
+
+const clickBtn = (id, path, timer) => {
+    chrome.scripting.executeScript(
+        {
+        args: [path, timer],
+        target: { tabId: id },
+        func: (arg1,  arg2) => {
+            let element = document.evaluate(
+                arg1,
+                document,
+                null,
+                XPathResult.FIRST_ORDERED_NODE_TYPE,
+                null,
+            ).singleNodeValue
+            setInterval(() => {
+                element.click()
+            }, arg2)
+        }
+    })
+}
---
+++ extension/foreground.js
@@ -0,0 +1,29 @@
+console.log("From foreground")
+
+const inputUrl = document.querySelector(".inputUrl");
+const inputRef = document.querySelector(".inputRef");
+const inputPathBtn = document.querySelector(".inputPathButton");
+const inputClick = document.querySelector(".inputClick");
+const startBtn = document.querySelector(".startBtn");
+const stopBtn = document.querySelector(".stopBtn");
+
+startBtn.addEventListener("click", () => {
+    chrome.tabs.query({url: `${inputUrl.value}/*`}, (tab) => {
+        chrome.runtime.sendMessage(
+            { 
+                tabIDMessage: tab[0].id,
+                options: {
+                    url: inputUrl.value,
+                    webRefreshTime: parseInt(inputRef.value),
+                    btnPath: inputPathBtn.value,
+                    btnClickTime: parseInt(inputClick.value),
+                },
+                message: "start"
+            }
+        );
+    })
+})
+
+stopBtn.addEventListener("click", () => {
+    chrome.runtime.sendMessage({message: "stop"});
+})
