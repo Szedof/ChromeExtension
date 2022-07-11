3c5b345d308a479898e93bc01eb29eb10c8b6423
Szedof <48103543+Szedof@users.noreply.github.com>
2022-07-11 12:29:36 +0200

    Update foreground.js

--- extension/foreground.js
+++ extension/foreground.js
@@ -9,6 +9,11 @@ const stopBtn = document.querySelector(".stopBtn");
 
 startBtn.addEventListener("click", () => {
     chrome.tabs.query({url: `${inputUrl.value}/*`}, (tab) => {
+
+        chrome.storage.local.set({"tabIDStorage": tab[0].id}, () => {
+            console.log(`${tab[0].id} set to local`)
+        })
+        
         chrome.runtime.sendMessage(
             { 
                 tabIDMessage: tab[0].id,
